2d37761e8c320b5a70a747b1e45c604739df8d03
Syd Bailey <sydabailey@gmail.com>
2019-07-19 04:35:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
-function happyHolidays ("Happy holidays!") {
-  console.log(happyHolidays)
+function happyHolidays () {
+  console.log("Happy holidays!")
 }
 
 function happyHolidaysTo (name) {
